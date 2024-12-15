fbc0e8e3d54b1c9c6894528caed19437caafe020
oihuegbu <ihuegbu@gmail.com>
2024-12-15 00:24:49 -0500

    edit project files

--- backend.tf
+++ backend.tf
@@ -7,10 +7,12 @@ terraform {
   }
 
   backend "s3" {
-    bucket         	   = "uchicago-tfstate"
+    //bucket         	   = "uchicago-tfstate"
+    bucket         	   = "unichicago-pipeline-tfstate"
     key              	   = "state/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
-    dynamodb_table = "uchicago_tf_lockid"
+    //dynamodb_table = "uchicago_tf_lockid"
+    dynamodb_table = "uchicago_pipeliine_tf_lockid"
   }
 }

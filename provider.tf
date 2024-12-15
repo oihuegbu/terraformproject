a4769184dd36c3fabcb4569bab55a13b13dd2f75
oihuegbu <ihuegbu@gmail.com>
2024-12-15 09:17:55 -0500

    edit project files

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 # provider info. Region set in variables.tf folder
 provider "aws" {
   region = "us-east-1"
-  access_key = local.ucaccesskey.unichiaccesskey
-  secret_key = local.ucsecretkey.unichisecretkey
+  //access_key = local.ucaccesskey.unichiaccesskey
+  access_key = $/UC_KEY/UC_ACCESS_KEY
+  secret_key = $/UC_SECRET/UC_SECRET_KEY
+  //secret_key = local.ucsecretkey.unichisecretkey
 }

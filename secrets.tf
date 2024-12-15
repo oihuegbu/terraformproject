aa44daa500e624c48b1470800bbb1c13650da563
oihuegbu <ihuegbu@gmail.com>
2024-12-14 23:44:32 -0500

    edit project files

--- secrets.tf
+++ secrets.tf
@@ -1,26 +1,26 @@
-resource "aws_secretsmanager_secret_version" "uchicago_aws_access_key" {
-  secret_id = "aws_access_key"
-}
+# resource "aws_secretsmanager_secret_version" "uchicago_aws_access_key" {
+#   secret_id = "aws_access_key"
+# }
 
-resource "aws_secretsmanager_secret_version" "uchicago_aws_secret_key" {
-  secret_id = "aws_secret_key"
-}
+# resource "aws_secretsmanager_secret_version" "uchicago_aws_secret_key" {
+#   secret_id = "aws_secret_key"
+# }
 
-resource "aws_secretsmanager_secret_version" "github_token" {
-  secret_id = "github token"
-}
+# resource "aws_secretsmanager_secret_version" "github_token" {
+#   secret_id = "github token"
+# }
 
-locals {
-  accesskey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_access_key.secret_string)
-}
+# locals {
+#   accesskey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_access_key.secret_string)
+# }
 
-locals {
-  secretkey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_secret_key.secret_string)
-}
+# locals {
+#   secretkey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_secret_key.secret_string)
+# }
 
-locals {
-  githubToken = jsondecode(aws_secretsmanager_secret_version.github_token.secret_string)
-}
+# locals {
+#   githubToken = jsondecode(aws_secretsmanager_secret_version.github_token.secret_string)
+# }
 
 
 

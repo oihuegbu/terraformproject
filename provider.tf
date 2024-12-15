1f0df3137664cdbdf675198082065b6f9c616683
oihuegbu <ihuegbu@gmail.com>
2024-12-15 00:45:35 -0500

    edit project files

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # provider info. Region set in variables.tf folder
 provider "aws" {
   region = "us-east-1"
-  access_key = local.accesskey.unichicago_access_key
-  secret_key = local.secretkey.unichicago_secret_key
+  access_key = local.ucaccesskey.unichiaccesskey
+  secret_key = local.ucsecretkey.unichisecretkey
 }
--- secrets.tf
+++ secrets.tf
@@ -1,22 +1,22 @@
-# resource "aws_secretsmanager_secret_version" "uchicago_aws_access_key" {
-#   secret_id = "aws_access_key"
-# }
+resource "aws_secretsmanager_secret_version" "uchicago_aws_access_key" {
+  secret_id = "unichiawsaccesskey"
+}
 
-# resource "aws_secretsmanager_secret_version" "uchicago_aws_secret_key" {
-#   secret_id = "aws_secret_key"
-# }
+resource "aws_secretsmanager_secret_version" "uchicago_aws_secret_key" {
+  secret_id = "unichiawssecretkey"
+}
 
 # resource "aws_secretsmanager_secret_version" "github_token" {
 #   secret_id = "github token"
 # }
 
-# locals {
-#   accesskey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_access_key.secret_string)
-# }
+locals {
+  ucaccesskey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_access_key.secret_string)
+}
 
-# locals {
-#   secretkey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_secret_key.secret_string)
-# }
+locals {
+  ucsecretkey = jsondecode(aws_secretsmanager_secret_version.uchicago_aws_secret_key.secret_string)
+}
 
 # locals {
 #   githubToken = jsondecode(aws_secretsmanager_secret_version.github_token.secret_string)

99b7cb64527ab393a30ed6391e0df4034c1bc559
oihuegbu <ihuegbu@gmail.com>
2024-12-15 09:45:59 -0500

    edit project files

--- provider.tf
+++ provider.tf
@@ -1,8 +1,17 @@
 # provider info. Region set in variables.tf folder
+
+data "aws_ssm_parameter" "unicbi_access_key" {
+  name = "/UC_KEY/UC_ACCESS_KEY"
+}
+
+data "aws_ssm_parameter" "unicbi_secret_key" {
+  name = "/UC_SECRET/UC_SECRET_KEY"
+}
+
 provider "aws" {
   region = "us-east-1"
   //access_key = local.ucaccesskey.unichiaccesskey
-  access_key = $/UC_KEY/UC_ACCESS_KEY
-  secret_key = $/UC_SECRET/UC_SECRET_KEY
+  access_key = data.aws_ssm_parameter.unicbi_access_key.value
+  secret_key = data.aws_ssm_parameter.unicbi_secret_key.value
   //secret_key = local.ucsecretkey.unichisecretkey
 }

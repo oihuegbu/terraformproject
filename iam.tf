ee20f85daffadcf1f7cde3b77a4b1d7402d05ead
oihuegbu <ihuegbu@gmail.com>
2024-12-15 20:09:14 -0500

    edit project files

--- iam.tf
+++ iam.tf
@@ -25,12 +25,101 @@ resource "aws_iam_policy_attachment" "lambda_policy_attachment"{
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# //iam role for codebuild
-# resource "aws_iam_role" "iam_for_codebuild" {
+//iam role for codepipeline
+resource "aws_iam_role" "uni_iam_for_codepipeline" {
+  name = "iam_for_unichicodepipeline"
 
-# }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
-# //iam policy for codebuild
-# resource "aws_iam_role_policy" "codebuild_policy" {
+    lifecycle {
+      prevent_destroy = true
+      ignore_changes = all
+    }
+}
 
-# }
+
+//iam policy for codepipeline
+resource "aws_iam_role_policy" "uni_codepipeline_policy" {
+    name = "unichicago_pipeline_policy"
+    role = aws_iam_role.uni_iam_for_codepipeline.id
+
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObject"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:GetLogEvents",
+        "logs:CreateLogStream",
+        "logs:CreateLogGroup"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:PutLogEvents",
+        "logs:GetLogEvents",
+        "logs:CreateLogStream",
+        "logs:CreateLogGroup"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codepipeline:GetPipeline"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:GetRolePolicy",
+        "iam:ListRolePolicies",
+        "iam:ListAttachedRolePolicies"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+    lifecycle {
+      prevent_destroy = true
+      ignore_changes = all
+    }
+}

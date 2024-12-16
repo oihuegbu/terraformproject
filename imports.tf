9509c7a6a5ef76e855077d3c779acfe026ae3459
oihuegbu <ihuegbu@gmail.com>
2024-12-15 20:49:18 -0500

    edit project files

--- imports.tf
+++ imports.tf
@@ -1,19 +1,19 @@
-import {
-    to = aws_iam_role.iam_for_codebuild
-    id = "iam_for_codebuild"
-}
+# import {
+#     to = aws_iam_role.iam_for_codebuild
+#     id = "iam_for_codebuild"
+# }
 
-import {
-    to = aws_iam_role.codepipeline
-    id = "iam_for_codepipeline"
-}
+# import {
+#     to = aws_iam_role.codepipeline
+#     id = "iam_for_codepipeline"
+# }
 
-import {
-    to = aws_codepipeline.codepipeline
-    id = "uni-chicago-pipeline"
-}
+# import {
+#     to = aws_codepipeline.codepipeline
+#     id = "uni-chicago-pipeline"
+# }
 
-import {
-    to = aws_codebuild_project.unichicago_build
-    id = "uni-chicago"
-}
+# import {
+#     to = aws_codebuild_project.unichicago_build
+#     id = "uni-chicago"
+# }

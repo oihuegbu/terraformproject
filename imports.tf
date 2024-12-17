61e725482deef3920e381df73baff921050ed82a
oihuegbu <ihuegbu@gmail.com>
2024-12-17 11:49:59 -0500

    updates

--- imports.tf
+++ imports.tf
@@ -1,35 +1,35 @@
-import {
-    to = aws_iam_role.iam_for_ucodebuild
-    id = "iam_for_codebuild"
-}
+# import {
+#     to = aws_iam_role.iam_for_ucodebuild
+#     id = "iam_for_codebuild"
+# }
 
-import {
-    to = aws_iam_role_policy.ucodebuild_policy
-    id = "uchicago_codebuild_policy"
-}
+# import {
+#     to = aws_iam_role_policy.ucodebuild_policy
+#     id = "uchicago_codebuild_policy"
+# }
 
-import {
-    to = aws_iam_role.iam_for_ucodepipeline
-    id = "iam_for_codepipeline"
-}
+# import {
+#     to = aws_iam_role.iam_for_ucodepipeline
+#     id = "iam_for_codepipeline"
+# }
 
-import {
-    to = aws_iam_role_policy.ucodepipeline_policy
-    id = "iam_for_codepipeline"
-}
+# import {
+#     to = aws_iam_role_policy.ucodepipeline_policy
+#     id = "iam_for_codepipeline"
+# }
 
-import {
-    to = aws_codepipeline.ucodepipeline
-    id = "uni-chicago-pipeline"
-}
+# import {
+#     to = aws_codepipeline.ucodepipeline
+#     id = "uni-chicago-pipeline"
+# }
 
-import {
-    to = aws_codebuild_project.uchicago_build
-    id = "uni-chicago"
-}
+# import {
+#     to = aws_codebuild_project.uchicago_build
+#     id = "uni-chicago"
+# }
 
-import {
-    to = aws_s3_bucket.ucodepipeline_bucket
-    id = "unichicago-codepipeline-bucket"
-}
+# import {
+#     to = aws_s3_bucket.ucodepipeline_bucket
+#     id = "unichicago-codepipeline-bucket"
+# }
 

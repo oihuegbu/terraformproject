0c918e7d0deae31579d4779929e472663d04baab
oihuegbu <ihuegbu@gmail.com>
2024-12-17 00:06:48 -0500

    updates

--- codebuild.tf
+++ codebuild.tf
@@ -1,31 +1,31 @@
-resource "aws_codebuild_project" "uchicago_build" {
-    name = "uni-chicago"
-    service_role = aws_iam_role.iam_for_ucodebuild.arn
+# resource "aws_codebuild_project" "uchicago_build" {
+#     name = "uni-chicago"
+#     service_role = aws_iam_role.iam_for_ucodebuild.arn
 
-    source {
-      type = "CODEPIPELINE"
-      location = "https://github.com/oihuegbu/terraformproject.git"
-      buildspec = "buildspec.yml"
-    }
+#     source {
+#       type = "CODEPIPELINE"
+#       location = "https://github.com/oihuegbu/terraformproject.git"
+#       buildspec = "buildspec.yml"
+#     }
 
-    environment {
-        compute_type = "BUILD_GENERAL1_SMALL"
-        image = "aws/codebuild/standard:7.0"
-        type = "LINUX_CONTAINER"
+#     environment {
+#         compute_type = "BUILD_GENERAL1_SMALL"
+#         image = "aws/codebuild/standard:7.0"
+#         type = "LINUX_CONTAINER"
 
-        environment_variable {
-          name = "deploy"
-          value = "0"
-        }
-    }
+#         environment_variable {
+#           name = "deploy"
+#           value = "0"
+#         }
+#     }
 
-    artifacts {
-      type = "CODEPIPELINE"
-    }
+#     artifacts {
+#       type = "CODEPIPELINE"
+#     }
 
-    lifecycle {
-      prevent_destroy = true
-      ignore_changes = all
-    }
+#     lifecycle {
+#       prevent_destroy = true
+#       ignore_changes = all
+#     }
 
-}
+# }

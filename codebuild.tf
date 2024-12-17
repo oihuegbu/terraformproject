8b206d7894e658c782eef96680390ed47244b4e8
oihuegbu <ihuegbu@gmail.com>
2024-12-17 01:04:45 -0500

    updates

--- codebuild.tf
+++ codebuild.tf
@@ -1,5 +1,5 @@
 
-resource "aws_codebuild_project" "uchicago_build" {
+resource "aws_codebuild_project" "unichicago_build" {
     name = "uni-chicago"
     service_role = aws_iam_role.iam_for_ucodebuild.arn
 
